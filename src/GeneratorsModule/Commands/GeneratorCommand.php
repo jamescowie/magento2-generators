78ba2a7dfab81dc099e51f1ce39b746f2cbc0f08
jamescowie <james@jcowie.co.uk>
2016-03-11 18:17:37 +0000

    Fix errors with namespace not loading

--- src/GeneratorsModule/Commands/GeneratorCommand.php
+++ src/GeneratorsModule/Commands/GeneratorCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Jcowie\Generators\Commands;
+namespace Jcowie\GeneratorsModule\Commands;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
